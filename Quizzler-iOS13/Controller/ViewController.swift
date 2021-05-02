04518bc66d856f30be2dfdd3a6dd93e9344cca4e
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-05-02 18:52:01 +0530

    Implemented : MVC architecture 🏄🏻‍♀️

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -10,23 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     var timer = Timer()
+    var quizBrain = QuizBrain()
     
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    
-    var index : Int = 0
-    var progress : Float = 0.25
-    var score : Int = 0
-    
-    let quiz = [
-        Question(q: "Is whale a mammal ? ", a: "True"),
-        Question(q:"Was rome built in one day ?", a: "False"),
-        Question(q: "Can chess be played with only one player ? ", a: "False"),
-        Question(q: "Are tigers and cats related ? ", a: "True")
-    ]
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,32 +24,26 @@ class ViewController: UIViewController {
     
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        let actualAnswer = quiz[index].answer
         let userAnswer = sender.currentTitle!
-                
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
         // increase the index and progress
-            if actualAnswer == userAnswer {
-                sender.backgroundColor = UIColor.green
-                updateScore()
-            }else{
-                sender.backgroundColor = UIColor.red
-            }
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        }else{
+            sender.backgroundColor = UIColor.red
+        }
+        quizBrain.nextQuestion()
         
         timer = Timer.scheduledTimer(timeInterval: 0.2 ,target: self,selector: #selector(updateUI),userInfo: nil,repeats: false)
     }
     
-   @objc func updateUI(){
-        progressBar.progress = progress
-        questionLabel.text = quiz[index].text
+    @objc func updateUI(){
+        progressBar.progress = quizBrain.getProgress()
+        questionLabel.text = quizBrain.getQuestionText()
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
-        index+=1
     }
-    
-    func updateScore(){
-       score+=1
-    }
-    
     
 }
 
